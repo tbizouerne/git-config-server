5db393962fb5af04ce0d1f257749e00bc62bf401
Thomas BIZOUERNE <thomas.bizouerne@gmail.com>
2017-05-01 02:10:34 +0200

    Project init.

---
+++ src/main/java/fr/blackhat/playing/springcloud/GitConfigServerApplication.java
@@ -0,0 +1,12 @@
+package fr.blackhat.playing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConfigServerApplication.class, args);
+	}
+}
